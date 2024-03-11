08d7f9921e34e43f577d5e549963832de8060920
2110107 <2110107@newuu.uz>
2024-03-11 12:50:25 +0500

    Feature - implemented logger messages in order to be able to monitor project

---
+++ src/main/java/com/epam/finalDemo/utils/LoggingAspect.java
@@ -0,0 +1,32 @@
+package com.epam.finalDemo.utils;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("within(@org.springframework.stereotype.Repository *)" + " || within(@org.springframework.stereotype.Service *)" + " || within(@org.springframework.web.bind.annotation.RestController *)" + " || within(@org.springframework.web.bind.annotation.ControllerAdvice *)")
+    public void loggingTargets() {
+    }
+
+    @AfterReturning(value = "loggingTargets()", returning = "value")
+    public void logAfterExecution(JoinPoint joinPoint, Object value) {
+        LOGGER.info("{} executed and returned value [{}]", joinPoint.getSignature().getName(), value);
+    }
+
+
+    @AfterThrowing(value = "loggingTargets()", throwing = "exception")
+    public void logAfterException(JoinPoint joinPoint, Exception exception) {
+        LOGGER.error("Method {}.{} threw {}. Exception: {}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), exception.getClass().getSimpleName(), exception.getMessage());
+    }
+
+}
